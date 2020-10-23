0227591ad7ccedc88c9099a481118b444a8c6626
nnkhoa <n.nhukhoa2209@gmail.com>
2020-10-23 11:47:08 +0200

    main: main changes to accomondate other changes in code

--- main.py
+++ main.py
@@ -6,45 +6,64 @@ import random
 import jsonl_parser
 import text_preprocess
 import text_features
+import data_seperation
 
 import pandas as pd
 
-n_sample = 20
 
-def load_raw_data(folder_path, lang):
-    list_file = [join(folder_path, f) for f in listdir(folder_path) if isfile(join(folder_path, f))]
+def load_raw_data(folder_path, trend_list, lang):
+    # all_file = [join(folder_path, f) for f in listdir(folder_path) if isfile(join(folder_path, f))]
+    n_sample = 40
+
+    list_file = [join(folder_path, f + ".jsonl") for f in trend_list if isfile(join(folder_path, f +  ".jsonl"))]
 
     data = []
+
+    if n_sample > len(trend_list):
+        n_sample = len(trend_list)
 
     for f in list_file[:n_sample]:
         data.extend(jsonl_parser.load_jsonl(f))
 
-    data_lang = [entry for entry in data if entry['lang'] == lang]
+    print() 
 
-    random.shuffle(data_lang)
+    data_lang = [entry for entry in data if entry['lang'] == lang]
 
     return data_lang
 
 
 def main():
     # data = load_raw_data(sys.argv[1])
-    data = load_raw_data('/Users/khoanguyen/Workspace/dataset/twitter-trending/TT-classification/dataset', 'en')
+    annotated = data_seperation.get_data('news')
+
+    trend_list = annotated['id'].tolist()
+    
+    data = load_raw_data('/Users/khoanguyen/Workspace/dataset/twitter-trending/TT-classification/dataset/', trend_list, 'en')
     
     df = pd.DataFrame(data)
 
+    df['trend_hash'].value_counts()
+
     df['text'] = df['text'].apply(lambda x: text_preprocess.remove_hyperlink(x))
 
+    df_trend = df[['text', 'trend_hash']]
+
+    trend_label = text_features.trend_mapping(set(df_trend['trend_hash'].tolist()))
+
+    df_trend['label'] = df_trend.apply(lambda row: trend_label[row.trend_hash], axis=1)
     # df['text'] = df['text'].apply(lambda x: text_preprocess.tokenize_text(x))
 
     # print(df.text.to_string(index=False))
 
-    text_features.generate_co_occurrences_matrix(df['text'], 1)
+    # text_features.generate_co_occurrences_matrix(df['text'], 1)
 
-    tf, features = text_features.generate_term_freq(df['text'], 2)
+    tf, features = text_features.generate_term_freq(df['text'], 1)
 
-    text_features.generate_tfidf(df['text'])
+    # text_features.generate_tfidf(df['text'])
 
-    text_features.lda_model(tf, features)
+    text_features.lda_model(tf, features, 24)
+
+    # text_features.k_mean_clustering(df_trend)
     
 if __name__ == '__main__':
     main()
