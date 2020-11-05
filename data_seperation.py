3e1c0d18ca0870043d96f1f7e95cee0c67f775c2
nnkhoa <n.nhukhoa2209@gmail.com>
2020-11-05 10:38:50 +0100

    data_seperation: unify dataset path for macbook and workstation

--- data_seperation.py
+++ data_seperation.py
@@ -3,18 +3,19 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join, splitext
 
-def get_data(type):
-    macbook_annotation_path='/Users/khoanguyen/Workspace/dataset/twitter-trending/TT-classification/TT-annotations.csv'
-    macbook_data_path='/Users/khoanguyen/Workspace/dataset/twitter-trending/TT-classification/dataset/'
+def get_data(trend_type):
+    annotation_path='TT-annotations.csv'
+    data_path='dataset/'
 
-    workstation_annotation_path = 'C:\\Users\\nnguyen\\Documents\\Twitter\\TT-annotations.csv'
-    workstation_data_path = 'C:\\Users\\nnguyen\\Documents\\Twitter\\dataset'
+    annotation_df = pd.read_csv(annotation_path, delimiter=';', names=['id', 'time', 'name', 'type'])
+    
+    if trend_type is not None:
+        trend_df = annotation_df[annotation_df['type'] == trend_type]
+    else:
+        trend_df = annotation_df
+    
+    list_file = [splitext(f)[0] for f in listdir(data_path) if isfile(join(data_path, f))]
 
-    annotation_df = pd.read_csv(workstation_annotation_path, delimiter=';', names=['id', 'time', 'name', 'type'])
-    news_df = annotation_df[annotation_df['type'] == type]
-
-    list_file = [splitext(f)[0] for f in listdir(workstation_data_path) if isfile(join(workstation_data_path, f))]
-
-    existing_trend = news_df[news_df['id'].isin(list_file)]
+    existing_trend = trend_df[trend_df['id'].isin(list_file)]
 
     return existing_trend
